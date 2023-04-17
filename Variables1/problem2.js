067ca78ec50f5b6159c006d48b2d9ca9ba2e2956
Shreya <you@example.com>
2023-04-17 21:46:03 +0530

    first commit

---
+++ Variables1/problem2.js
@@ -0,0 +1,6 @@
+var name="Shreya";
+console.log(name);
+name="Dilip";
+console.log(name);
+name="kalpana";
+console.log(name);
---
+++ Variables1/problem4.js
@@ -0,0 +1,22 @@
+let name="shreya";
+let school="New English School";
+let grade="12";
+let section="A";
+let roll_no=121;
+let science=76;
+let math=82;
+let english=78;
+
+
+console.log("*********Report card*********");
+console.log();
+console.log("name:",name);
+console.log("school:",school);
+console.log("grade",grade);
+console.log("section",section);
+console.log("roll_no",roll_no);
+console.log("science marks:",science);
+console.log("math marks",math);
+console.log("english marks:",english);
+console.log();
+console.log("*****************************");
